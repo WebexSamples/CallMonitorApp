690efb466e66a5c4a890841533e1a9666e9c4172
Adam Weeks <adweeks@cisco.com>
2023-06-02 16:08:23 -0400

    feat(Calls): show call state flow

---
+++ src/CallStateFlow.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './CallStateFlow.css'; // Import the CSS file for styling
+
+const CallStateFlow = ({ callState }) => {
+  const getSegmentClass = (segmentState) => {
+    return callState === segmentState ? 'progress-segment active' : 'progress-segment';
+  };
+
+  const getStateLabel = (callState) => {
+    switch (callState) {
+      case 'Started':
+        return 'Call Started';
+      case 'Connecting':
+        return 'Connecting...';
+      case 'Connected':
+        return 'Call Connected';
+      case 'Disconnecting':
+        return 'Disconnecting...';
+      case 'Disconnected':
+        return 'Call Disconnected';
+      case 'Rejected':
+        return 'Call Rejected';
+      case 'Hold':
+        return 'Call on Hold';
+      case 'Ended':
+        return 'Call Ended';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <div className="call-state-flow">
+      <div className="progress-bar">
+        <div className={getSegmentClass('Started')}></div>
+        <div className={getSegmentClass('Connecting')}></div>
+        <div className={getSegmentClass('Connected')}></div>
+        <div className={getSegmentClass('Disconnecting')}></div>
+        <div className={getSegmentClass('Disconnected')}></div>
+        <div className={getSegmentClass('Rejected')}></div>
+        <div className={getSegmentClass('Hold')}></div>
+        <div className={getSegmentClass('Ended')}></div>
+      </div>
+      <div className="state-label">{getStateLabel(callState)}</div>
+    </div>
+  );
+};
+
+export default CallStateFlow;
--- src/Calls.js
+++ src/Calls.js
@@ -1,3 +1,5 @@
+import CallStateFlow from "./CallStateFlow";
+
 function Call({call, onShow}) {
   return (
     <div className="row">
@@ -9,13 +11,15 @@ function Call({call, onShow}) {
             <p><b>ID:</b> {call.id}</p>
             <p><b>Local Participant:</b> {call.localParticipant.callerID} ({call.localParticipant.name}) - {call.localParticipant.isMuted ? 'Muted' : 'Not Muted'}</p>
             <p><b>Remote Participants:</b> {call.remoteParticipants.map(rp => `${rp.callerID} (${rp.name})`).join(', ')}</p>
-            <p><b>State:</b> {call.state}</p>
             <p><b>Last Update:</b> {call.eventTimeStamp?.toString()}</p>
           </div>
           <div className="card-action">
             <button className="waves-effect waves-light btn" onClick={onShow}>
               Search Remote Caller
             </button>
+          </div>
+          <div className="card-action">
+            <CallStateFlow callState={call.state} />
           </div>
         </div>
       </div>
